24e89e866f0b9524701b928752ff899a7ac79577
shinnosuke tamai <76773901+ShinnosukeTamai@users.noreply.github.com>
2021-01-06 20:48:28 +0900

    Update myled.c

--- myled.c
+++ myled.c
@@ -5,7 +5,7 @@
 #include <linux/uaccess.h>
 #include <linux/io.h>
 
-MODULE_AUTHOR("Ryuichi Ueda");
+MODULE_AUTHOR("Ryuichi Ueda　and Shinnosuke Tamai");
 MODULE_DESCRIPTION("driver for LED control");
 MODULE_LICENSE("GPL");
 MODULE_VERSION("0.0.1");
